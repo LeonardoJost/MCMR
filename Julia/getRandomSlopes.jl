7cbc1e23499dee9a786a7186e4b2f6dcd1fc6538
Leonardo Jost <leo1989@gmx.de>
2021-05-23 12:11:09 +0200

    Updated random slope calculation

--- Julia/getRandomSlopes.jl
+++ Julia/getRandomSlopes.jl
@@ -27,13 +27,13 @@ dataset=CSV.read("dataset\\dataset.csv", DataFrame)
 #inspect data
 show(first(dataset,6))
 show(names(dataset))
-show(IOContext(stdout, :limit=>false),dataset[115:1000,[:type, :deg]])
+#show(IOContext(stdout, :limit=>false),dataset[115:1000,[:type, :deg]])
 show(eltype.(eachcol(dataset)))
 #remove outliers
 dataset=dataset[.!dataset.outlier,:]
 dataset=dataset[dataset[!,:sex].!="NA",:]
 #convert types to numeric
-for i in [:sexNumeric]
+for i in [:sexContrasts]
   dataset[!,i]=tryparse.(Int,dataset[!,i])
 end
 #add block as numeric variable
@@ -45,174 +45,130 @@ dataset.responseCorrect=dataset.type.=="hit"
 ##random slopes
 #start with maximal model
 #(do not use interactions to avoid overparametrization, check later for interactions of interest)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              (nStimuliFactor+typeNumeric+block+deg+itemNumber|ID)+
-              (sexNumeric+nStimuliFactor+typeNumeric+block+STEM+Experience+deg+itemNumber|modelNumber))
+              (nStimuliFactor+typeContrasts+block+deg+itemNumber|ID)+
+              (sexContrasts+nStimuliFactor+typeContrasts+block+STEM+Experience+deg+itemNumber|modelNumber))
 @elapsed gm1=fit(MixedModel,modelFormula,dataset,Binomial())
 show(gm1.optsum)
 #remove random correlation
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric+block+deg+itemNumber|ID)+
-              zerocorr(sexNumeric+nStimuliFactor+typeNumeric+block+STEM+Experience+deg+itemNumber|modelNumber))
+              zerocorr(nStimuliFactor+typeContrasts+block+deg+itemNumber|ID)+
+              zerocorr(sexContrasts+nStimuliFactor+typeContrasts+block+STEM+Experience+deg+itemNumber|modelNumber))
 @elapsed gm2=fit(MixedModel,modelFormula,dataset,Binomial())
 show(VarCorr(gm2))
 show(MixedModels.likelihoodratiotest(gm1,gm2))
 #gm2 is better than gm1
-#remove (deg+type+itemNumber+sexNumeric|modelNumber) (variance <.001)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+#remove (deg+itemNumber|ID)+(deg+type+itemNumber+sexContrasts|modelNumber) (variance <.001)
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric+block+deg+itemNumber|ID)+
-              zerocorr(nStimuliFactor+block+STEM+Experience|modelNumber))
+              zerocorr(nStimuliFactor+typeContrasts+block|ID)+
+              zerocorr(sexContrasts+nStimuliFactor+block+STEM+Experience|modelNumber))
 @elapsed gm3=fit(MixedModel,modelFormula,dataset,Binomial())
 show(MixedModels.likelihoodratiotest(gm2,gm3))
 show(VarCorr(gm3))
 #gm3 is better
-#remove (deg+itemNumber|ID) (variance <.001)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+#remove (sexContrasts+STEM+nStimuliFactor+block|modelNumber) (variance/components <.002)
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric+block|ID)+
-              zerocorr(nStimuliFactor+block+STEM+Experience|modelNumber))
+              zerocorr(nStimuliFactor+typeContrasts+block|ID)+
+              zerocorr(Experience|modelNumber))
 @elapsed gm4=fit(MixedModel,modelFormula,dataset,Binomial())
 show(MixedModels.likelihoodratiotest(gm3,gm4))
 show(VarCorr(gm4))
 #gm4 is better
-#remove (block+STEM|modelNumber) (variance components <.001)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+#test removal of (block|ID) (lowest variance component)
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric+block|ID)+
-              zerocorr(nStimuliFactor+Experience|modelNumber))
+              zerocorr(nStimuliFactor+typeContrasts|ID)+
+              zerocorr(Experience|modelNumber))
 @elapsed gm5=fit(MixedModel,modelFormula,dataset,Binomial())
 show(MixedModels.likelihoodratiotest(gm4,gm5))
-show(VarCorr(gm5))
-#gm5 is better
-#test removal (nStimuliFactor|modelNumber) (lowest variance components)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric+block|ID)+
-              zerocorr(Experience|modelNumber))
-@elapsed gm6=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm5,gm6))
-#gm5 is better
-#test removal (Experience|modelNumber) (low variance components)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric+block|ID)+
-              zerocorr(nStimuliFactor|modelNumber))
-@elapsed gm6a=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm5,gm6a))
-#gm5 is better
-#test removal (typeNumeric|ID) (low variance components)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+#gm4 is better
+#test removal (typeContrasts|ID) (lowest variance)
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
               zerocorr(nStimuliFactor+block|ID)+
-              zerocorr(nStimuliFactor+Experience|modelNumber))
-@elapsed gm7=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm5,gm7))
-#gm5 is better
-#test removal (nStimuliFactor|ID)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              zerocorr(typeNumeric+block|ID)+
-              zerocorr(nStimuliFactor+Experience|modelNumber))
-@elapsed gm8=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm5,gm8))
-#gm5 is better
-#test removal (block|ID)
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              zerocorr(nStimuliFactor+typeNumeric|ID)+
-              zerocorr(nStimuliFactor+Experience|modelNumber))
-@elapsed gm9=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm5,gm9))
-#gm5 is better
+              zerocorr(Experience|modelNumber))
+@elapsed gm6=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm4,gm6))
+#gm4 is better
+
 #readd random correlation
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              (nStimuliFactor+typeNumeric+block|ID)+
-              (nStimuliFactor+Experience|modelNumber))
-@elapsed gm10=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm5,gm10))
-#gm10 is good
+              (nStimuliFactor+typeContrasts+block|ID)+
+              (Experience|modelNumber))
+@elapsed gm7=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm4,gm7))
+#gm7 is good
 #test for removal of individual random slopes
 #nStimuliFactor|ID
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              (typeNumeric+block|ID)+
-              (nStimuliFactor+Experience|modelNumber))
-@elapsed gm11=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm10,gm11))
-#typeNumeric|ID
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+              (typeContrasts+block|ID)+
+              (Experience|modelNumber))
+@elapsed gm8=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm8))
+#typeContrasts|ID
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
               (nStimuliFactor+block|ID)+
-              (nStimuliFactor+Experience|modelNumber))
-@elapsed gm12=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm10,gm12))
-#block|ID
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              (nStimuliFactor+typeNumeric|ID)+
-              (nStimuliFactor+Experience|modelNumber))
-@elapsed gm13=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm10,gm13))
-#Experience|modelNumber
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              (nStimuliFactor+typeNumeric+block|ID)+
-              (nStimuliFactor|modelNumber))
-@elapsed gm14=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm10,gm14))
-#nStimuliFactor|modelNumber
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
-              block+deg+itemNumber+
-              (nStimuliFactor+typeNumeric+block|ID)+
               (Experience|modelNumber))
-@elapsed gm15=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm10,gm15))
-#gm15 is best model
+@elapsed gm9=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm9))
+#block|ID
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
+              block+deg+itemNumber+
+              (nStimuliFactor+typeContrasts|ID)+
+              (Experience|modelNumber))
+@elapsed gm10=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm10))
+#Experience|modelNumber
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
+              block+deg+itemNumber+
+              (nStimuliFactor+typeContrasts+block|ID)+
+              (1|modelNumber))
+@elapsed gm11=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm11))
+#gm7 is best model
 
 #reduce nStimuli and block to numeric instead of factors
 #nStimuli
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              (nStimuli+typeNumeric+block|ID)+
+              (nStimuli+typeContrasts+block|ID)+
               (Experience|modelNumber))
-@elapsed gm16=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm15,gm16))
+@elapsed gm12=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm12))
 #block
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              (nStimuliFactor+typeNumeric+blockNumeric|ID)+
+              (nStimuliFactor+typeContrasts+blockNumeric|ID)+
               (Experience|modelNumber))
-@elapsed gm17=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm15,gm17))
-#gm15 is best
+@elapsed gm13=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm13))
+#gm7 is best
 #check for random slope of interaction
-modelFormula=@formula(responseCorrect~STEM*sexNumeric*nStimuliFactor*typeNumeric+
-              Experience*sexNumeric*nStimuliFactor*typeNumeric+
+modelFormula=@formula(responseCorrect~STEMContrasts2*ExperienceContrasts*sexContrasts*nStimuliFactor*typeContrasts+
+              STEMContrasts1*sexContrasts*nStimuliFactor*typeContrasts+
               block+deg+itemNumber+
-              (nStimuliFactor*typeNumeric+block|ID)+
+              (nStimuliFactor*typeContrasts+block|ID)+
               (Experience|modelNumber))
-@elapsed gm18=fit(MixedModel,modelFormula,dataset,Binomial())
-show(MixedModels.likelihoodratiotest(gm15,gm18))
-#gm15 is better
+@elapsed gm14=fit(MixedModel,modelFormula,dataset,Binomial())
+show(MixedModels.likelihoodratiotest(gm7,gm14))
+#gm7 is best
